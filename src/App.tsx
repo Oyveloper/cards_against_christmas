9afa019ccc4b82f36b3f2113fc05758d8ae9067b
Øyvind Monsen <yvind.monsen@gmail.com>
2020-12-15 17:34:53 +0100

    Removing any game logic from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import React from "react";
 import "./App.css";
-import Card, { CardType } from "./core/Card";
-import Game from "./core/Game";
-import Player from "./core/Player";
 
 import GameScreen from "./Screens/GameScreen";
 
 function App() {
-  let players: Player[] = [];
-
-  for (var i = 0; i < 4; i++) {
-    players.push(new Player(`Player${i}`));
-  }
-
-  const game: Game = new Game("123", players, new Player("local"));
-
-  for (i = 0; i < 7; i++) {
-    game.drawCard();
-  }
-
-  game.setJudge(game.players[0]);
-  game.drawBlackCard();
-
   return (
     <div className="App">
-      <GameScreen game={game} />
+      <GameScreen id="123" playerName="Bob" />
     </div>
   );
 }
