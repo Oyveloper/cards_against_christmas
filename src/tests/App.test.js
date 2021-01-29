43ec391ae0f3f98b953304167cc8872a4220276a
Øyvind Monsen <yvind.monsen@gmail.com>
2021-01-29 20:29:08 +0100

    Fixing the test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import App from "../App";
 
 it("renders correctly", () => {
-  const div = document.createAttribute("div");
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 });
